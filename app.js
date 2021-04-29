ed862d7d73a93896a10f1066935dc71496b6f670
C0d3president <kadimasam@gmail.com>
2021-04-29 16:09:06 +0300

    Created a function to calculate the volume of milk per shade

---
+++ app.js
@@ -0,0 +1,44 @@
+// MILK PRODUCED PER SHADE PER DAY
+
+let oneShadeMilk = (cows, volume)=>{
+
+    this.volume = volume;
+    this.cows = cows;
+    
+    return this.volume * this.cows;
+};
+
+// TOTAL AMOUNT OF MILK FOR ALL SHADES PER DAY
+
+let allShadeMilk = (oneShadeMilk, shades)=>{
+   
+    this.shades = shades;
+
+    return oneShadeMilk * this.shades;
+};
+
+// INCOME PER DAY
+const price = 45;
+let incomePerDay = (allShadeMilk, price)=>{
+    
+    return allShadeMilk * price;
+}
+
+// INCOME PER WEEK
+
+function incomePerWeek(incomePerDay){
+    
+    return incomePerDay * 7;
+}
+
+// INCOME PER MONTH
+
+function incomePerMonth(incomePerDay,numberOfDays){
+    return incomePerDay * numberOfDays;
+}
+
+// INCOME PER YEAR
+
+function incomePerYear(incomePerMonth,numberOfMonths){
+    return incomePerMonth * numberOfMonths;
+}
