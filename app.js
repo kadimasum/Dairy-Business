3a46701917de7f3d1316d273cdd1384a2276f478
C0d3president <kadimasam@gmail.com>
2021-04-30 23:43:25 +0300

    Add a function produceReport and an event listener to prevent sefault action on submit

--- app.js
+++ app.js
@@ -1,44 +1,6 @@
-// MILK PRODUCED PER SHADE PER DAY
+function produceReport(){
 
-let oneShadeMilk = (cows, volume)=>{
+    () => Event.preventDefault();
 
-    this.volume = volume;
-    this.cows = cows;
-    
-    return this.volume * this.cows;
-};
-
-// TOTAL AMOUNT OF MILK FOR ALL SHADES PER DAY
-
-let allShadeMilk = (oneShadeMilk, shades)=>{
    
-    this.shades = shades;
-
-    return oneShadeMilk * this.shades;
-};
-
-// INCOME PER DAY
-const price = 45;
-let incomePerDay = (allShadeMilk, price)=>{
-    
-    return allShadeMilk * price;
-}
-
-// INCOME PER WEEK
-
-function incomePerWeek(incomePerDay){
-    
-    return incomePerDay * 7;
-}
-
-// INCOME PER MONTH
-
-function incomePerMonth(incomePerDay,month.days){
-    return incomePerDay * numberOfDays;
-}
-
-// INCOME PER YEAR
-
-function incomePerYear(incomePerMonth,numberOfMonths){
-    return incomePerMonth * numberOfMonths;
 }
