62e1f6691ecf60ff51a42bf6bdc01aa43444bc60
C0d3president <kadimasam@gmail.com>
2021-05-01 01:44:22 +0300

    Add lines to app.js to get form data

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 function produceReport(){
 
-    () => Event.preventDefault();
+    Event.preventDefault();
 
     let data = {};
 
-    data["shed-A"] = parseInt(document.getElementById("shed-A").value);
-    data["shed-B"] = parseInt(document.getElementById("shed-A").value);
-    data["shed-C"] = parseInt(document.getElementById("shed-A").value);
-    data["shed-D"] = parseInt(document.getElementById("shed-A").value);
+    data["shed_A"] = parseInt(document.getElementById("shed_A").value);
+    data["shed_B"] = parseInt(document.getElementById("shed_B").value);
+    data["shed_C"] = parseInt(document.getElementById("shed_C").value);
+    data["shed_D"] = parseInt(document.getElementById("shed_D").value);
+
+    sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D;
+
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + "litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + "litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + "litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + "litres per day</p>";
+
+    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + "litres per day</p>";
 }
+
