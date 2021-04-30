9e9bdc5c86b9af40e994d391cef030c97c2c6c32
C0d3president <kadimasam@gmail.com>
2021-04-30 23:45:34 +0300

    Add a variable to app.js to store form data

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ function produceReport(){
 
     () => Event.preventDefault();
 
-   
+    let data = {};
+
+    data["shed-A"] = parseInt(document.getElementById("shed-A").value);
+    data["shed-B"] = parseInt(document.getElementById("shed-A").value);
+    data["shed-C"] = parseInt(document.getElementById("shed-A").value);
+    data["shed-D"] = parseInt(document.getElementById("shed-A").value);
 }
