a48f5887f1313e162a23689ce89c287de8a3fb9b
C0d3president <kadimasam@gmail.com>
2021-05-01 02:42:18 +0300

    Change color of displayOutput container and add box shadow

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function produceReport(){
 
-    Event.preventDefault();
+    event.preventDefault();
 
     let data = {};
 
@@ -9,13 +9,13 @@ function produceReport(){
     data["shed_C"] = parseInt(document.getElementById("shed_C").value);
     data["shed_D"] = parseInt(document.getElementById("shed_D").value);
 
-    sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D;
+    let sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D;
 
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + "litres per day</p>";
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + "litres per day</p>";
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + "litres per day</p>";
-    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + "litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + " litres per day</p>";
 
-    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + "litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres per day</p>";
 }
 
